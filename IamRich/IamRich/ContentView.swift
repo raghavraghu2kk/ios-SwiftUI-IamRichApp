11197cd0839c4ac66e253d9db63f568a97152188
Raghavendra Mirajkar <raghavendramirajkar@Raghavendras-MacBook-Air.local>
2024-03-16 23:32:18 +0530

    Intial project setup

---
+++ IamRich/IamRich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IamRich
+//
+//  Created by Raghavendra Mirajkar on 16/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IamRich/IamRich/IamRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IamRichApp.swift
+//  IamRich
+//
+//  Created by Raghavendra Mirajkar on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct IamRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
